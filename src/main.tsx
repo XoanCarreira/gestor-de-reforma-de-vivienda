0ae597fcf140e68a311e617194587df4c944eb96
XoanCarreira <xoancano@gmail.com>
2026-07-09 20:50:05 +0200

    revisión sw

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,19 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-);
 
+);
 serviceWorkerRegistration.register();
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(registration => {
+        console.log('Service worker rexistrado:', registration);
+      })
+      .catch(error => {
+        console.error('Erro ao rexistrar o service worker:', error);
+      });
+  });
+}
+
