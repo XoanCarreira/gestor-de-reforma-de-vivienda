8ba14f474c21c101f0dc44c1aa6f7d32134dd675
XoanCarreira <xoancano@gmail.com>
2026-07-09 20:36:14 +0200

    corrixe erro en path sw

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
